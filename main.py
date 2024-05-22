f7afa25a8a1a046ed5bdba46496b0a3d44be7602
Mattey2906 <matthias.pfrendl@stud.fh-campuswien.ac.at>
2024-05-22 14:48:47 +0200

    Data Vis 2

--- main.py
+++ main.py
@@ -32,6 +32,8 @@ print(f"PNEUMONIA={len(os.listdir(os.path.join(val_dir, 'PNEUMONIA')))}")
 print(f"NORMAL={len(os.listdir(os.path.join(val_dir, 'NORMAL')))}")
 print(" ")
 
+#---------------- Data Visualization: PNEUMONIA -----------------
+
 pneumonia_dir = "input/chest_xray/train/PNEUMONIA"
 pneumonia_files = [file for file in os.listdir(pneumonia_dir) if file.lower().endswith(('.png', '.jpg', '.jpeg'))]
 
@@ -45,3 +47,32 @@ for i in range(min(9, len(pneumonia_files))):  # Limit the loop to the number of
 
 plt.tight_layout()
 plt.show()
+
+#---------------- Data Visualization: NORMAL -----------------
+
+normal_dir = "input/chest_xray/train/NORMAL"
+normal_files = [file for file in os.listdir(normal_dir) if file.lower().endswith(('.png', '.jpg', '.jpeg'))]
+
+plt.figure(figsize=(20, 10))
+
+for i in range(9):
+    plt.subplot(3, 3, i + 1)
+    img = plt.imread(os.path.join(normal_dir, normal_files[i]))
+    plt.imshow(img, cmap='gray')
+    plt.axis('off')
+
+plt.tight_layout()
+plt.show()
+
+#sample image, image = 10
+normal_img = os.listdir("input/chest_xray/train/NORMAL")[10]
+sample_img = plt.imread(os.path.join(normal_dir, normal_img))
+plt.imshow(sample_img, cmap='gray')
+plt.colorbar()
+plt.title('Raw Chest X Ray Image')
+
+print(f"The dimensions of the image are {sample_img.shape[0]} pixels width and {sample_img.shape[1]} pixels height, one single color channel.")
+print(f"The maximum pixel value is {sample_img.max():.4f} and the minimum is {sample_img.min():.4f}")
+print(f"The mean value of the pixels is {sample_img.mean():.4f} and the standard deviation is {sample_img.std():.4f}")
+
+#---------------- Image Pre Processing -----------------
