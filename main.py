e2c786ee989f30072f6820361a5049d6ce65f131
Mattey2906 <matthias.pfrendl@stud.fh-campuswien.ac.at>
2024-05-24 10:03:30 +0200

    Image Preprocessing startet

--- main.py
+++ main.py
@@ -64,15 +64,61 @@ for i in range(9):
 plt.tight_layout()
 plt.show()
 
-#sample image, image = 10
-normal_img = os.listdir("input/chest_xray/train/NORMAL")[10]
+#sample image, image
+pic_nr = 15
+pic_path = "input/chest_xray/train/NORMAL"
+normal_img = os.listdir(pic_path)[pic_nr]
 sample_img = plt.imread(os.path.join(normal_dir, normal_img))
 plt.imshow(sample_img, cmap='gray')
 plt.colorbar()
 plt.title('Raw Chest X Ray Image')
 
+print(f"Sample Picture {pic_nr} loaded from {pic_path}")
 print(f"The dimensions of the image are {sample_img.shape[0]} pixels width and {sample_img.shape[1]} pixels height, one single color channel.")
 print(f"The maximum pixel value is {sample_img.max():.4f} and the minimum is {sample_img.min():.4f}")
 print(f"The mean value of the pixels is {sample_img.mean():.4f} and the standard deviation is {sample_img.std():.4f}")
 
+# Nutze histplot anstelle von distplot
+
+plt.figure(figsize=(10, 6))
+sns.histplot(sample_img.ravel(), kde=False, bins=32,
+             label=f"Pixel Mean {np.mean(sample_img):.4f} & Standard Deviation {np.std(sample_img):.4f}")
+plt.legend(loc='upper center')
+plt.title('Distribution of Pixel Intensities in the Image')
+plt.xlabel('Pixel Intensity')
+plt.ylabel('# Pixels in Image')
+plt.xlim(0, 255)  # Begrenzung der x-Achse auf typische Pixelwerte
+plt.show()
+
 #---------------- Image Pre Processing -----------------
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+print("\n\n +++ Image Preprocessing start:")
+
+
+image_generator = ImageDataGenerator(
+    rotation_range=20,
+    width_shift_range=0.1,
+    shear_range=0.1,
+    zoom_range=0.1,
+    samplewise_center=True,
+    samplewise_std_normalization=True
+)
+
+train = image_generator.flow_from_directory(train_dir,
+                                            batch_size=8,
+                                            shuffle=True,
+                                            class_mode='binary',
+                                            target_size=(180, 180))
+
+validation = image_generator.flow_from_directory(val_dir,
+                                                batch_size=1,
+                                                shuffle=False,
+                                                class_mode='binary',
+                                                target_size=(180, 180))
+
+test = image_generator.flow_from_directory(test_dir,
+                                            batch_size=1,
+                                            shuffle=False,
+                                            class_mode='binary',
+                                            target_size=(180, 180))
