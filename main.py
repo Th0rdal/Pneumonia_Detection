880f2036a213f5f3ddbd00c533c3108d3a977a9d
Mattey2906 <matthias.pfrendl@stud.fh-campuswien.ac.at>
2024-06-16 01:42:25 +0200

    imports

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-# This is a sample Python script.
+import tensorflow as tf
+from keras.datasets import mnist
+import cv2 #module: opencv-python 4.10.0.82 (neueste pip version muss installiert sein)
+import os
+import pathlib
+from keras.layers import Conv2D, Conv2DTranspose, Dropout, Dense, Reshape, LayerNormalization, LeakyReLU
+from keras import layers, models
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import f1_score, recall_score, precision_score
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
