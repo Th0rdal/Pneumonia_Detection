e86d848f871a430c2dcd7c61d04030829b1e416d
Mattey2906 <matthias.pfrendl@stud.fh-campuswien.ac.at>
2024-06-16 13:27:37 +0200

    read Images

--- main.py
+++ main.py
@@ -10,3 +10,34 @@ import numpy as np
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.metrics import f1_score, recall_score, precision_score
 
+class ReadDataset:
+    def __init__(self, datasetpath, labels, image_shape):
+        self.datasetpath = datasetpath
+        self.labels = labels
+        self.image_shape = image_shape
+    def returListImages(self,):
+        self.images = []
+        for label in self.labels:
+            self.images.append(list(pathlib.Path(os.path.join(self.datasetpath,
+                                                              label)).glob('*.*')))
+    def readImages(self,):
+        self.returListImages()
+        self.finalImages = []
+        labels = []
+        for label in range(len(self.labels)):
+            for img in self.images[label]:
+                img = cv2.imread(str(img))
+                img = cv2.resize(img , self.image_shape)
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+                img  = img/255
+                self.finalImages.append(img)
+                labels.append(label)
+        images = np.array(self.finalImages)
+        labels = np.array(labels)
+        return images, labels
+
+readDatasetObject = ReadDataset('/input/chest-xray-pneumonia/chest_xray/train',
+                                    ['NORMAL', 'PNEUMONIA'],
+                                    (64, 64)) #180 x 180 eigentlich bei unserem Recognition Model
+images, labels = readDatasetObject.readImages()
+images.shape, labels.shape
