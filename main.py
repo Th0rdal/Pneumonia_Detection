a6a84f0e00e0a56bbf3db24a76c1e505e18a257d
Mattey2906 <matthias.pfrendl@stud.fh-campuswien.ac.at>
2024-06-18 18:14:26 +0200

    Acgan

--- main.py
+++ main.py
@@ -7,8 +7,14 @@ from keras.layers import Conv2D, Conv2DTranspose, Dropout, Dense, Reshape, Layer
 from keras import layers, models
 import matplotlib.pyplot as plt
 import numpy as np
+
+import pydot
+from tensorflow.keras.utils import plot_model
+
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.metrics import f1_score, recall_score, precision_score
+
+# ----------------------------------------------------------------------
 
 class ReadDataset:
     def __init__(self, datasetpath, labels, image_shape):
@@ -29,7 +35,7 @@ class ReadDataset:
                 img = cv2.imread(str(img))
                 img = cv2.resize(img , self.image_shape)
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                img  = img/255
+                img = img/255.0
                 self.finalImages.append(img)
                 labels.append(label)
         images = np.array(self.finalImages)
@@ -38,7 +44,152 @@ class ReadDataset:
 
 readDatasetObject = ReadDataset('input/chest_xray/train',
                                     ['NORMAL', 'PNEUMONIA'],
-                                    (64, 64)) #180 x 180 eigentlich bei unserem Recognition Model
+                                    (60, 60)) #180 x 180 eigentlich bei unserem Recognition Model
 images, labels = readDatasetObject.readImages()
 print(f'Images: Amount={images.shape[0]}, {images.shape[1]}x{images.shape[2]}, Channels={images.shape[3]}')
 print(f'Labels: Amount={labels.shape[0]}')
+
+# ----------------------------------------------------------------------
+
+plt.figure(figsize = (12, 12))
+indexs = np.random.randint(0, len(labels), size = (64, ))
+for i in range(64):
+    plt.subplot(8, 8, (i + 1))
+    plt.imshow(images[indexs[i]])
+    plt.title(labels[indexs[i]])
+plt.tight_layout()  # Optional: automatic tight Layout for better space usage
+plt.show()  # show the generated plot
+
+# ----------------------------------------------------------------------
+class Acgan:
+    def __init__(self, eta, batch_size, epochs, weight_decay, latent_space,
+                 image_shape, kernel_size):
+        self.eta = eta
+        self.batch_size = batch_size
+        self.epochs = epochs
+        self.weight_decay = weight_decay
+        self.latent_space = latent_space
+        self.image_shape = image_shape
+        self.kernel_size = kernel_size
+    def data(self, images, labels):
+        ytrain = tf.keras.utils.to_categorical(labels)
+        self.images = images
+        self.labels = ytrain
+    def samples(self, G, noize, labels):
+        images = G.predict([noize, labels])
+        ys = np.argmax(labels, axis = 1)
+        plt.figure(figsize = (12, 4))
+        for i in range(16):
+            plt.subplot(2, 8, (i + 1))
+            plt.imshow(images[i], cmap = 'gray')
+            plt.title(ys[i])
+        plt.show()
+    def generator(self, inputs, labels):
+        filters = [256, 128, 64, 32]
+        padding = 'same'
+        x = inputs
+        y = labels
+        x = layers.concatenate([x, y])
+        x = layers.Dense(1024, )(x)
+        x = layers.Dense(8*8*filters[0],
+                         kernel_regularizer = tf.keras.regularizers.L2(0.001))(x)
+        x = layers.Reshape((8, 8, filters[0]))(x)
+        for filter in filters:
+            if filter >= 64:
+                strides = 2
+            else:
+                strides = 1
+            x = LayerNormalization()(x)
+            x = layers.Activation('relu')(x)
+            x = Conv2DTranspose(filter, kernel_size = self.kernel_size, padding = padding,
+                      strides = strides)(x)
+        x = Conv2DTranspose(3, kernel_size = self.kernel_size, padding = padding)(x)
+        x = layers.Activation('sigmoid')(x)
+        self.generatorModel = models.Model(inputs = [inputs, labels],
+                                           outputs = x,
+                                           name = 'generator')
+    def discriminator(self, inputs):
+        x = inputs
+        filters = [32, 64, 128, 256]
+        padding = 'same'
+        for filter in filters:
+            if filter < 256:
+                strides = 2
+            else:
+                strides = 1
+            x = Conv2D(filter, kernel_size = self.kernel_size, padding = padding,
+                      strides = strides,
+                      kernel_regularizer = tf.keras.regularizers.L2(0.001))(x)
+            x = LeakyReLU(alpha = 0.2)(x)
+        x = layers.Flatten()(x)
+        outputs = Dense(1, )(x)
+        labelsOutput = Dense(256,
+                             kernel_regularizer = tf.keras.regularizers.L2(0.001))(x)
+        labelsOutput = Dropout(0.3)(labelsOutput)
+        labelsOutput = Dense(2,)(labelsOutput)
+        labelsOutput = layers.Activation('softmax')(labelsOutput)
+        self.discriminatorModel = models.Model(inputs = inputs,
+                                               outputs = [outputs, labelsOutput],
+                                               name = 'discriminator')
+    def build(self,):
+        generatorInput = layers.Input(shape=(self.latent_space,))  # Korrigiert: Tupel (100,)
+        discriminatorInput = layers.Input(shape = (self.image_shape))
+        labelsInput = layers.Input(shape = (2, ))
+        self.generator(generatorInput, labelsInput)
+        self.discriminator(discriminatorInput)
+        G = self.generatorModel
+        D = self.discriminatorModel
+        D.compile(loss = ['mse', 'binary_crossentropy'],
+                 optimizer = tf.keras.optimizers.RMSprop(learning_rate = self.eta,
+                                                        weight_decay = self.weight_decay))
+        D.summary()
+        G.summary()
+        D.trainable = False
+        GAN = models.Model(inputs = [generatorInput, labelsInput],
+                           outputs = D(G([generatorInput, labelsInput])))
+        GAN.compile(loss = ['mse', 'binary_crossentropy'],
+                   optimizer = tf.keras.optimizers.RMSprop(learning_rate = self.eta*0.5,
+                                                          weight_decay = self.weight_decay*0.5))
+        GAN.summary()
+        return G, D, GAN
+    def trainAlgorithm(self, G, D, GAN):
+        for epoch in range(self.epochs):
+            indexs = np.random.randint(0, len(self.images), size=(self.batch_size,))
+            realImages = self.images[indexs]
+            realLabels = self.labels[indexs]
+            realTag = tf.ones(shape=(self.batch_size,))
+            noize = tf.random.uniform(shape=(self.batch_size, self.latent_space), minval=-1, maxval=1)
+            fakeLabels = tf.keras.utils.to_categorical(np.random.choice(range(2), size=(self.batch_size)),
+                                                       num_classes=2)
+            fakeImages = tf.squeeze(G.predict([noize, fakeLabels], verbose=0))
+
+            # Resize real and fake images to the same size
+            realImages_resized = tf.image.resize(realImages, (self.image_shape[0], self.image_shape[1]))
+            fakeImages_resized = tf.image.resize(fakeImages, (self.image_shape[0], self.image_shape[1]))
+
+            fakeTag = tf.zeros(shape=(self.batch_size,))
+            allImages = np.vstack([realImages_resized, fakeImages_resized])
+            allLabels = np.vstack([realLabels, fakeLabels])
+            allTags = np.hstack([realTag, fakeTag])
+
+            print(f'Epoch: {epoch}, allImages.shape: {allImages.shape}, allLabels.shape: {allLabels.shape}, allTags.shape: {allTags.shape}')
+
+            _, dlossTag, dlossLabels = D.train_on_batch(allImages, [allTags, allLabels])
+            noize = tf.random.uniform(shape=(self.batch_size, self.latent_space), minval=-1, maxval=1)
+            _, glossTag, glossLabels = GAN.train_on_batch([noize, fakeLabels], [realTag, fakeLabels])
+            if epoch % 5000 == 0:
+                print('Epoch: {}'.format(epoch))
+                print(
+                    'discriminator loss: [tag: {}, labels: {}], generator loss: [tag: {}, labels: {}]'.format(dlossTag,
+                                                                                                              dlossLabels,
+                                                                                                              glossTag,
+                                                                                                              glossLabels))
+                self.samples(G, noize, fakeLabels)
+
+acgan = Acgan(eta=0.0001, batch_size=32, epochs=32000, weight_decay=6e-9, latent_space=100, image_shape=(64, 64, 3), kernel_size=5)
+
+acgan.data(images, labels)
+
+G, D, GAN = acgan.build()
+
+acgan.trainAlgorithm(G, D, GAN)
